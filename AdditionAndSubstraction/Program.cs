3a977d4cfe92583217220d83b59fdd402734b5ea
Fatih <r.fatihercan@hotmail.com>
2022-11-20 21:45:04 +0300

    Add project files.

---
+++ AdditionAndSubstraction/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+int sum = 0;
+for (int i = 0; i <= 10; i++)
+{
+    if (i % 2 == 1)
+    {
+        sum += i;
+    }
+
+}
+Console.WriteLine(sum);
+sum = 0;
+for (int i = 10; i >= 0; i--)
+{
+    if (i % 2 == 0)
+    {
+        sum += i;
+    }
+
+}
+Console.WriteLine(sum);
